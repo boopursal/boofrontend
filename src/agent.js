798fef804d96b9ed75c537122bc8c8f76ea6d9e1
7e-sky <webmaster@7e-sky.com>
2024-07-16 16:05:40 +0100

    Update agent.js

--- src/agent.js
+++ src/agent.js
@@ -4,5 +4,5 @@ export default axios.create({
   //baseURL: `http://192.168.1.124:8000/`
   //baseURL: `http://192.168.11.108:8000/`,
  // baseURL: `https://it.3findustrie.com/`,
-    baseURL: `https://ha.7e-sky.com/`,
+    baseURL: `https://dev.3findustrie.com/`,
 });
