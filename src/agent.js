4599d1798e79de6ec83ab543d5a12bf83df59d8e
boopursal <webmaster@boopursal.com>
2024-11-12 11:01:08 +0100

    Update agent.js

--- src/agent.js
+++ src/agent.js
@@ -4,5 +4,5 @@ export default axios.create({
   //baseURL: `http://192.168.1.124:8000/`
   //baseURL: `http://192.168.11.108:8000/`,
  // baseURL: `https://it.3findustrie.com/`,
-    baseURL: `https://git.3findustrie.com/`,
+    baseURL: `https://api.boopursal.com/`,
 });
