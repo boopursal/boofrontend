b4109d3026339a2c175c34c3766f8fb970c23a00
boopursal <webmaster@boopursal.com>
2024-11-13 09:28:05 +0100

    Update Constants.js

--- src/@fuse/Constants.js
+++ src/@fuse/Constants.js
@@ -1,4 +1,4 @@
-export const URL_SITE = "https://api.boopursal.com";
+export const URL_SITE = "https://it.3findustrie.com";
 export const FRONT_URL = "https://www.boopursal.com";
 export const LOCAL_CURRENCY = "MAD";
 export const LOCAL_TVA = 0.2;
