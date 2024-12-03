ebf705e33b47a49f1d13789ee075f2a351920b26
boopursal <webmaster@boopursal.com>
2024-12-03 17:14:22 +0100

    Update FooterLayout3.js

--- src/app/fuse-layouts/layout3/components/FooterLayout3.js
+++ src/app/fuse-layouts/layout3/components/FooterLayout3.js
@@ -19,7 +19,7 @@ function FooterLayout3(props) {
                                 à propos de nous
                             </Typography>
                             <Typography className="text-justify">
-                                Les Achats Industriels est une place de marché b2b qui permet aux Acheteurs et aux Fournisseurs de se rencontrer dans une même plate-forme (électronique).
+                                Boopursal est une place de marché b2b qui permet aux Acheteurs et aux Fournisseurs de se rencontrer dans une même plate-forme (électronique).
                                 <br /><br /> Nous nous positionnons en tant qu'intermédiaire entre les deux parties afin de faciliter aux uns et aux autres de vendre ou/et acheter de manière fluide, économique, rapide, sans limites géographiques.
                             </Typography>
 
