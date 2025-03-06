c5701e6a344d9ba3cad96ce7a62637ef86d4706f
7e-sky <webmaster@boopursal.com>
2025-03-06 11:30:40 +0000

    achCon

--- src/app/main/acheteur/acheteurConfigs.js
+++ src/app/main/acheteur/acheteurConfigs.js
@@ -3,20 +3,42 @@ import { BlackListesAppConfig } from "./black_liste/BlackListesAppConfig";
 import { ProfileConfigs } from "./profile/ProfileConfigs";
 import { DashboardAppConfig } from "./dashboard/DashboardAppConfig";
 //import AcheteursPage from './AcheteursPage';
-import { ImportConfigs } from "./import/ImportConfigs";
+import ImportConfigs from "./import/ImportConfigs";
 import { TeamsAppConfig } from "./Directeur_achat/TeamsAppConfig";
 import { childsConfigs } from "./childs/childsConfigs";
-import { SuggestionsAppConfig } from "./Demande_suggetion/SuggestionsAppConfig";
 import { FacturationAppConfig } from "./facturation/FacturationAppConfig";
+import { authRoles } from 'app/auth';
+
+// Créer la configuration des routes d'acheteur
+const AcheteurConfigsObject = {
+    settings: {
+        layout: {}
+    },
+    auth: authRoles.acheteur,
+    routes: [
+        {
+            path: '/ac/import',
+            component: ImportConfigs
+        },
+        ...demandeConfigs.routes,
+        ...BlackListesAppConfig.routes,
+        ...ProfileConfigs.routes,
+        ...TeamsAppConfig.routes,
+        ...childsConfigs.routes,
+        ...DashboardAppConfig.routes,
+        ...FacturationAppConfig.routes
+
+    ]
+};
+
+// Exporter la configuration sous forme de tableau comme attendu par routesConfig.js
 export const acheteurConfigs = [
+    AcheteurConfigsObject,
     demandeConfigs,
     BlackListesAppConfig,
     ProfileConfigs,
     TeamsAppConfig,
     childsConfigs,
-    SuggestionsAppConfig,
-    ImportConfigs,
-    FacturationAppConfig,
-   // AcheteursPage,
-    DashboardAppConfig
+    DashboardAppConfig,
+    FacturationAppConfig
 ];
