37b3dc4e1d17b698aeec7f5a72d99a79ed106177
7e-sky <webmaster@boopursal.com>
2025-04-22 15:43:20 +0100

    Newsle

--- src/app/main/portail/index/Newsletter.js
+++ src/app/main/portail/index/Newsletter.js
@@ -1,60 +1,69 @@
-import React, { useEffect, useState } from 'react';
-import MailchimpSubscribe from "react-mailchimp-subscribe";
-import { Grid, Button, Icon, Paper, Typography } from '@material-ui/core';
+import React from 'react';
+import MailchimpSubscribe from "react-mailchimp-subscribe"
+import { Grid, Button, Icon, Paper } from '@material-ui/core';
+
 
 const CustomForm = ({ status, message, onValidated }) => {
-    const [timeoutError, setTimeoutError] = useState(false);
     let email;
-
-    const submit = () => {
-        if (email && email.value.indexOf("@") > -1) {
-            setTimeoutError(false); // Réinitialiser l'erreur de timeout
-            onValidated({ EMAIL: email.value });
-            setTimeout(() => {
-                if (status === "sending") {
-                    setTimeoutError(true);
-                }
-            }, 10000); // 10 secondes
-        } else {
-            alert("Veuillez entrer une adresse e-mail valide.");
-        }
-    };
+    const submit = () =>
+        email &&
+        email.value.indexOf("@") > -1 &&
+        onValidated({
+            EMAIL: email.value,
+        });
 
     return (
-        <div style={{ display: "inline-block", width: '100%' }}>
-            {timeoutError && <div style={{ color: "red" }}>Erreur : L'envoi a expiré. Veuillez réessayer.</div>}
-            {status === "sending" && <div style={{ color: "blue" }}>Mail envoyé avec succès</div>}
-            {/* {status === "error" && (
-                <div style={{ color: "red" }} dangerouslySetInnerHTML={{ __html: message }} />
-            )} */}
+        <div
+            style={{
+                display: "inline-block",
+                width: '100%',
+            }}
+        >
+            {status === "sending" && <div style={{ color: "blue" }}>Envoi en cours...</div>}
+            {status === "error" && (
+                <div
+                    style={{ color: "red" }}
+                    dangerouslySetInnerHTML={{ __html: message }}
+                />
+            )}
             {status === "success" && (
-                <div style={{ color: "green" }} dangerouslySetInnerHTML={{ __html: message }} />
+                <div
+                    style={{ color: "green" }}
+                    dangerouslySetInnerHTML={{ __html: message }}
+                />
             )}
             <Grid container spacing={2}>
                 <Grid item sm={8}>
-                    <Paper className="flex p-4 items-center mx-auto w-full max-w-640" elevation={1}>
+                    <Paper className="flex p-4 items-center mx-auto w-full max-w-640 " elevation={1}>
+
                         <Icon className="mr-8 ml-8" color="action">mail_outline</Icon>
+
                         <input
                             className="h-28 w-full"
                             ref={node => (email = node)}
                             type="email"
                             placeholder="exemple@exemple.com"
                         />
+
                     </Paper>
+
                 </Grid>
                 <Grid item sm={4}>
                     <Button className="whitespace-no-wrap" color="secondary" onClick={submit} variant="contained">
                         Soumettre
                     </Button>
+
                 </Grid>
             </Grid>
+
         </div>
     );
 };
 
-export default function Newsletter() {
-    const url = "https://lesachatsindustriels.us4.list-manage.com/subscribe/post?u=f32aaf1c3e01b891b2db4b37a&id=fee05cc375";
-    
+// use the render prop and your custom form
+export default function Newsletter(props) {
+    const url = "https://boopursal.us1.list-manage.com/subscribe/post?u=ec057da8878f92c766693bd65&amp;id=8947f5aa1f&amp;f_id=00c07de2f0";
+
     return (
         <MailchimpSubscribe
             url={url}
@@ -66,5 +75,6 @@ export default function Newsletter() {
                 />
             )}
         />
-    );
+    )
 }
+
